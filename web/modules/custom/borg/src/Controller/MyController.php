2b03cc14cdf50c4781b37e383de4ccd43504833f
Borg <bogdan.petruk15@gmail.com>
2021-07-27 17:40:24 +0300

    creating custom module

---
+++ web/modules/custom/borg/src/Controller/MyController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\borg\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Provides route responses for the Example module.
+ */
+class MyController extends ControllerBase {
+
+  /**
+   * Returns a simple page.
+   *
+   * @return array
+   *   A simple renderable array.
+   */
+  public function myNewPage() {
+    return [
+      '#markup' => "Hello! You can add here a photo of your cat.",
+    ];
+  }
+
+}
