d792ed8a16b407d4881da364ba8beb9879724bd6
Borg <bogdan.petruk15@gmail.com>
2021-08-06 12:29:38 +0300

    render output image

--- web/modules/custom/borg/src/Form/DatabaseOutput.php
+++ web/modules/custom/borg/src/Form/DatabaseOutput.php
@@ -3,26 +3,39 @@
 namespace Drupal\borg\Form;
 
 use Drupal\Core\Database\Database;
+use Drupal\file\Entity\File;
 
 class DatabaseOutput extends Database {
 
   public function DatabaseOutput() {
     $connect = Database::getConnection();
     $output = $connect->select('borg', 'x')
-      ->fields('x', ['cat_name', 'email', 'image', 'time'])
+      ->fields('x', ['image', 'cat_name', 'email', 'time'])
       ->execute();
-    $results = $output->fetchAll(\PDO::FETCH_ASSOC);
+    $results = $output->fetchAllAssoc('time', \PDO::FETCH_ASSOC);
     $results = array_reverse($results);
     $rows = [];
-
+    $i = 0;
     foreach ($results as $value) {
-      array_push($rows, $value);
+      $file = File::load($value['image']);
+      $value['image'] = [
+        '#type' => 'image',
+        '#theme' => 'image_style',
+        '#style_name' => 'large',
+        '#uri' => $file->getFileUri(),
+      ];
+      $renderer = \Drupal::service('renderer');
+      $value['image'] = $renderer->render($value['image']);
+      $value['time'] = date('j F Y H:i:s', $value['time']);
+      $rows[$i] = $value;
+      $i += 1;
     }
-
     $content = [
       '#type' => 'table',
       '#rows' => $rows,
-      '#empty' => t('No entries available.'),
+      '#attributes' => [
+        'class' => 'my-custom-table',
+      ],
     ];
     return $content;
   }
